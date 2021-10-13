ea5f43dcfbb0a99e443f21366d169175d9e3f84c
Alex Cabanillas <64493715+dducken@users.noreply.github.com>
2021-10-12 21:18:32 -0300

    challenge-alkemy

---
+++ DisneyAPI/Resultados/ResultadoAPI.cs
@@ -0,0 +1,11 @@
+namespace Resultados
+{
+    public class ResultadoAPI
+    {
+        public bool Ok {get; set;}
+        public string Error {get; set;}
+        public int CodigoError {get; set;}
+        public object Return {get; set;}
+        
+    }
+}
